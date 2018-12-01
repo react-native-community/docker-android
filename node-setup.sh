3abe882ee41092d5b4415d98e7cbb2b8d9f91f31
Dulmandakh <dulmandakh@gmail.com>
2018-12-02 00:53:21 +0800

    node-setup.sh: add comments and make more readable

--- node-setup.sh
+++ node-setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "deb https://deb.nodesource.com/node_10.x stretch main" > /etc/apt/sources.list.d/nodesource.list \
-&& echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list \
-&& curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add - \
-&& curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-&& apt-get update \
-&& apt-get install -y --no-install-recommends nodejs yarn \
-&& rm -rf /var/lib/apt/lists/*
+# add node 10.x repo and key to apt sources
+echo "deb https://deb.nodesource.com/node_10.x stretch main" > /etc/apt/sources.list.d/nodesource.list
+apt-key adv --fetch-keys https://deb.nodesource.com/gpgkey/nodesource.gpg.key
+
+# add yarn official repo and key to apt sources
+echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
+apt-key adv --fetch-keys https://dl.yarnpkg.com/debian/pubkey.gpg
+
+apt-get update \
+apt-get install -y --no-install-recommends \
+    nodejs \
+    yarn
+rm -rf /var/lib/apt/lists/*
