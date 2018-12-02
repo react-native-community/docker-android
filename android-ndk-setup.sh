1ad89c09f0000f98684e8c624a2059994967bda2
Dulmandakh <dulmandakh@gmail.com>
2018-12-02 09:27:14 +0800

    fix unzip in ndk-setup

--- android-ndk-setup.sh
+++ android-ndk-setup.sh
@@ -4,5 +4,5 @@ export NDK_VERSION=17c
 
 curl -sS https://dl.google.com/android/repository/android-ndk-r$NDK_VERSION-linux-x86_64.zip -o /tmp/ndk.zip
 mkdir -p $ANDROID_NDK
-unzip -q /tmp/ndk.zip -d $ANDROID_NDK
+unzip -q -d $ANDROID_NDK /tmp/ndk.zip
 rm /tmp/ndk.zip
