8c39d3b841dc7cfda3e190feaa96847d9bb954f4
Dulmandakh <dulmandakh@gmail.com>
2018-12-02 14:18:56 +0800

    fix ndk setup

--- android-ndk-setup.sh
+++ android-ndk-setup.sh
@@ -3,6 +3,6 @@
 export NDK_VERSION=17c
 
 curl -sS https://dl.google.com/android/repository/android-ndk-r$NDK_VERSION-linux-x86_64.zip -o /tmp/ndk.zip
-mkdir -p $ANDROID_NDK
-unzip -q -d $ANDROID_NDK /tmp/ndk.zip
+unzip -q -d /tmp /tmp/ndk.zip
+mv /tmp/android-ndk-r${NDK_VERSION} $ANDROID_NDK
 rm /tmp/ndk.zip
