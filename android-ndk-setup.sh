85d60ab88b3fb42d78252f35ce5836b3ceef3d21
Dulmandakh <dulmandakh@gmail.com>
2018-12-02 09:05:04 +0800

    create $ANDROID_NDK dir

--- android-ndk-setup.sh
+++ android-ndk-setup.sh
@@ -3,5 +3,6 @@
 export NDK_VERSION=17c
 
 curl -sS https://dl.google.com/android/repository/android-ndk-r$NDK_VERSION-linux-x86_64.zip -o /tmp/ndk.zip
+mkdir -p $ANDROID_NDK
 unzip -q /tmp/ndk.zip -d $ANDROID_NDK
 rm /tmp/ndk.zip
