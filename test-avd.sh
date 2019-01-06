52dac6baee7190b621d046eb271a76847b5ae762
gengjiawen <technicalcute@gmail.com>
2019-01-06 16:52:09 +0800

    fix emulator and add related ci
    Signed-off-by: gengjiawen <technicalcute@gmail.com>

---
+++ test-avd.sh
@@ -0,0 +1,5 @@
+echo no | avdmanager create avd -n testEmulator -k "system-images;android-19;default;armeabi-v7a"
+emulator -avd testEmulator -no-audio -no-window &
+curl -sS https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator -o android-wait-for-emulator
+chmod +x ./android-wait-for-boot.sh && ./android-wait-for-boot.sh
+adb shell input keyevent 82
