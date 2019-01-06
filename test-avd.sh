c8fad9eaed306c66fc64319244a664655f53d115
gengjiawen <technicalcute@gmail.com>
2019-01-06 19:52:58 +0800

    fix script
    Signed-off-by: gengjiawen <technicalcute@gmail.com>

--- test-avd.sh
+++ test-avd.sh
@@ -1,4 +1,5 @@
-echo no | avdmanager create avd -n testEmulator -k "system-images;android-19;default;armeabi-v7a"
+set -e
+echo no | avdmanager create avd -n testEmulator -k "system-images;android-19;google_apis;armeabi-v7a"
 emulator -avd testEmulator -no-audio -no-window &
 curl -sS https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator -o android-wait-for-emulator
 chmod +x ./android-wait-for-boot.sh && ./android-wait-for-boot.sh
