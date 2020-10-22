89d8e0e7f934282b6f2a31c14959b140962296c3
Jiawen Geng <technicalcute@gmail.com>
2020-10-22 09:22:58 +0800

    CI: cleanup circle CI (#89)

--- publish_docker.js
+++ publish_docker.js
@@ -9,7 +9,6 @@ docker login -u ${process.env.DOCKER_USER} -p ${process.env.DOCKER_PASS}
 docker build -t ${repo}:${finaltag} .
 docker tag ${repo}:${finaltag} ${repo}
 docker push ${repo}:${finaltag}
-docker push ${repo}
   `
   cmd.trim().split("\n").map(i => {
     const cmd = i.trim();
