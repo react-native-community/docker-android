428ca2368643cb9203e65782251187a5fda97a30
Nicola Corti <corti.nico@gmail.com>
2022-03-08 23:31:57 +0000

    Add a `CMAKE_BIN_PATH` variable to the PATH (#164)
    * Add a `CMAKE_BIN_PATH` variable to the PATH
    
    * Configure `KOTLIN_HOME`

--- scripts/test-react-native-setup.sh
+++ scripts/test-react-native-setup.sh
@@ -2,12 +2,16 @@
 
 set -e
 
-echo "Check Buck setup"
+export KOTLIN_HOME="third-party/kotlin"
+
+echo "Download Buck dependencies"
 ./scripts/circleci/buck_fetch.sh
+
+echo "Build React Native via Buck"
 buck build ReactAndroid/src/main/java/com/facebook/react
 buck build ReactAndroid/src/main/java/com/facebook/react/shell
 
-echo "Build React Native"
+echo "Build React Native via Gradle"
 yarn install
 ./gradlew --no-daemon :ReactAndroid:packageReactNdkLibsForBuck
 
